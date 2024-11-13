1246dc4a51368c2c1a53948420b55707029de482
Rachele Barberis <rachele.barberis.12@gmail.com>
2024-11-13 15:11:53 +0100

    buon punto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,38 @@
-const frmLis=document.getElementById('myList');
-const insertItem= document.getElementById('insertItem');
+const frmList=document.getElementById('myList');
+const insertItem=document.getElementById('insertItem');
 const btnInsert=document.getElementById('btnInsert');
+const listItems=document.getElementById('listItem');
+
+const list= [];
+
+btnInsert.addEventListener('click', function (e) {
+    e.preventDefault();
+    arrayCompleto();
+    printList();
+    frmList.reset();
+}
+);
+
+function arrayCompleto () {
+    list.push(insertItem.value);
+}
+function printList() {
+    listItems.innerHTML="";
+    for (let i=0; i<list.length; i++) {
+        let newLi=document.createElement('li');
+        newLi.classList.add('taskElement');
+        newLi.innerText=list[i];
+        let btnCanc=document.createElement('button');
+        btnCanc.setAttribute('type','button');
+        btnCanc.setAttribute('onclick',`deleteItem(${i})`);
+        btnCanc.innerText='☑️';
+        newLi.appendChild(btnCanc);
+        listItems.appendChild(newLi);
+    }
+
+}
+
+function deleteItem(index) {
+    list.splice(index,1);
+    printList();
+}
